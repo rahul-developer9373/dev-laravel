18878a40124c22f5f9b90495b9d5151588a96b5a
rahul <developer.rahul.9373@gmail.com>
2018-12-18 17:33:36 +0530

    laravel 18Dec2018

---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Testing</div>
+
+                <div class="panel-body">
+                   Super Kisan dashboard                   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
